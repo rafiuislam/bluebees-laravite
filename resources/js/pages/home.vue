17b8a68f70c00875625c772a539195869fc411e1
venturexrepo <mobarakali62@gmail.com>
2022-11-13 12:18:16 +0600

    team ui

--- resources/js/pages/home.vue
+++ resources/js/pages/home.vue
@@ -89,54 +89,78 @@
 
     <!-- project section -->
     <section class="project-section">
+        <div class="project-left-bg"></div>
         <div class="container">
             <div class="row">
                 <div class="col-md-4">
-                    <div class="project_item">
-                        <h1>Our Project</h1>
-                        <p>
-                            Sit amet, consectetur adipiscing elit, sed do
-                            eiusmod tempor incididunt ut labore et dolore magna
-                            aliqua. Quis ipsum suspendisse amet, consectetur
-                            adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore et dolore magna aliqua.
-                        </p>
+                    <div class="project-item">
+                        <div class="project-item-box first">
+                            <h1>Our Project</h1>
+                            <p>
+                                Sit amet, consectetur adipiscing elit, sed do
+                                eiusmod tempor incididunt ut labore et dolore
+                                magna aliqua. Quis ipsum suspendisse amet,
+                                consectetur adipiscing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna
+                                aliqua.
+                            </p>
+                        </div>
                     </div>
                 </div>
                 <div class="col-md-4">
-                    <img
-                        src="../../images/project-pic-1.jpg"
-                        class="image_project"
-                        alt="project"
-                    />
+                    <div class="project-item">
+                        <div class="project-item-box">
+                            <img
+                                src="../../images/project-pic-1.jpg"
+                                class="image_project"
+                                alt="project"
+                            />
+                        </div>
+                    </div>
                 </div>
                 <div class="col-md-4">
-                    <img
-                        src="../../images/project-pic-2.jpg"
-                        class="image_project"
-                        alt="project"
-                    />
+                    <div class="project-item">
+                        <div class="project-item-box">
+                            <img
+                                src="../../images/project-pic-2.jpg"
+                                class="image_project"
+                                alt="project"
+                            />
+                        </div>
+                    </div>
                 </div>
                 <div class="col-md-4">
-                    <img
-                        src="../../images/project-pic-3.jpg"
-                        class="image_project"
-                        alt="project"
-                    />
+                    <div class="project-item">
+                        <div class="project-item-box">
+                            <img
+                                src="../../images/project-pic-3.jpg"
+                                class="image_project"
+                                alt="project"
+                            />
+                        </div>
+                    </div>
                 </div>
                 <div class="col-md-4">
-                    <img
-                        src="../../images/project-pic-4.jpg"
-                        class="image_project"
-                        alt="project"
-                    />
+                    <div class="project-item">
+                        <div class="project-item-box">
+                            <img
+                                src="../../images/project-pic-4.jpg"
+                                class="image_project"
+                                alt="project"
+                            />
+                        </div>
+                    </div>
                 </div>
                 <div class="col-md-4">
-                    <img
-                        src="../../images/project-pic-5.jpg"
-                        class="image_project"
-                        alt="project"
-                    />
+                    <div class="project-item">
+                        <div class="project-item-box">
+                            <img
+                                src="../../images/project-pic-5.jpg"
+                                class="image_project"
+                                alt="project"
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -145,25 +169,61 @@
     <!-- Team section -->
 
     <div class="team">
-        <h1 class="d-flex justify-content-center align-self-center">
-            Our Team
-        </h1>
+        <div class="team-right-bg"></div>
+        <div class="container">
+            <h1 class="title">Our Team</h1>
 
-        <p>
-            We are a team of UI/UX designers who are passionate about creating
-            beautiful and functional designs. viverra maecenas accumsan lacus
-            vel facilisis. Dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
-        <button class="btn">See More Our Teamwork</button>
-        <div
-            class="service_item d-flex flex-column justify-content-start align-items-center"
-        >
-            <h2>BlueBees AI</h2>
             <p>
-                We are a team of UI/UX designers who are passionate about
-                creating beautiful and functional designs.
+                Sed tempor eget leo ut gravida. Quisque congue sagittis purus,
+                sit amet frinfringilla sapien quis, tempor id, scelerisque
+                Sed<br />
+                tempor eget leo ut gravida. Quisque congue sagittis purus, sit
+                amet frinfringilla sapien quis, tempor id, scelerisque Sed<br />
+                tempor eget leo ut gravida. Quisque congue sagittis purus,
             </p>
+            <div class="text-center team-work">
+                <button class="btn">See More Our Team work</button>
+            </div>
+            <div class="team-img text-center">
+                <img class="img-fluid" src="../../images/team.png" alt="" />
+            </div>
+
+            <div class="info-wrap">
+                <div class="info-box">
+                    <div class="row">
+                        <div class="col-md-7">
+                            <div
+                                class="info-text h-100 d-flex align-items-center"
+                            >
+                                <div>
+                                    <h2>BlueBees ai</h2>
+                                    <p>
+                                        We are a tech-based organization
+                                        partnered with a Singaporean based
+                                        company that is ready to assist
+                                        customers from all over the world with
+                                        state-of-the-art services they can rely
+                                        on. We created The BlueBees.ai with the
+                                        idea of making it easy for small and
+                                        upcoming innovative businesses to access
+                                        professional services they can rely on.
+                                        Our commitment is to help these
+                                        businesses grow and expand in rewarding
+                                        ways.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-5">
+                            <img
+                                class="img-fluid"
+                                src="../../images/info.png"
+                                alt=""
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
