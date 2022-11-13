a3416199a9f816cbaa937448b3b8783431f0bb85
Rafiu Islam <md.rafiu.islam@g.bracu.ac.bd>
2022-11-13 10:40:09 +0600

    updated sections

--- resources/js/pages/home.vue
+++ resources/js/pages/home.vue
@@ -11,196 +11,211 @@
             </div>
         </div>
     </div>
-    <div class="container">
-        <!-- service section -->
 
-        <div class="service">
-            <h1 class="d-flex justify-content-center align-self-center">
-                Our Services
-            </h1>
-            <div class="service_container">
-                <div>
-                    <div>
-                        <img
-                            src="../../images/ui-ux-design-icon.png"
-                            alt="ui-ux"
-                        />
+    <!-- service section -->
+    <section class="service-section">
+        <div class="container">
+            <h2 class="title">Our Services</h2>
+            <div class="service-container">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="service-item text-center">
+                            <div class="service-icon">
+                                <img
+                                    src="../../images/ui-ux-design-icon.png"
+                                    alt="ui-ux"
+                                />
+                            </div>
+                            <h3>UI UX <br />Design</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore
+                            </p>
+                        </div>
                     </div>
-                    <h3>
-                        UI UX <br />
-                        Design
-                    </h3>
-                    <p>We are a team of UI/UX designers who are passionate</p>
-                </div>
-                <div>
-                    <div>
-                        <img
-                            src="../../images/web-application-icon.png"
-                            alt="web-app"
-                        />
+                    <div class="col-md-3">
+                        <div class="service-item text-center">
+                            <div class="service-icon">
+                                <img
+                                    src="../../images/ui-ux-design-icon.png"
+                                    alt="ui-ux"
+                                />
+                            </div>
+                            <h3>UI UX <br />Design</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore
+                            </p>
+                        </div>
                     </div>
-                    <h3>
-                        Web Application<br />
-                        Development
-                    </h3>
-                    <p>
-                        We are a team of UI/UX designers who are passionate
-                        functional designs.
-                    </p>
-                </div>
-                <div>
-                    <div>
-                        <img
-                            src="../../images/custom-software.png"
-                            alt="custom-software"
-                        />
+                    <div class="col-md-3">
+                        <div class="service-item text-center">
+                            <div class="service-icon">
+                                <img
+                                    src="../../images/ui-ux-design-icon.png"
+                                    alt="ui-ux"
+                                />
+                            </div>
+                            <h3>UI UX <br />Design</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore
+                            </p>
+                        </div>
                     </div>
-                    <h3>
-                        Custom Software <br />
-                        Development
-                    </h3>
-                    <p>
-                        We are a team of UI/UX designers who are passionate
-                        functional designs.
-                    </p>
-                </div>
-                <div>
-                    <div>
-                        <img
-                            src="../../images/Mobile-Application.png"
-                            alt="mobile-app"
-                        />
+                    <div class="col-md-3">
+                        <div class="service-item text-center">
+                            <div class="service-icon">
+                                <img
+                                    src="../../images/ui-ux-design-icon.png"
+                                    alt="ui-ux"
+                                />
+                            </div>
+                            <h3>UI UX <br />Design</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore
+                            </p>
+                        </div>
                     </div>
-                    <h3>
-                        Mobile Application <br />
-                        Development
-                    </h3>
-                    <p>
-                        We are a team of UI/UX designers about creating
-                        beautiful and functional designs.
-                    </p>
                 </div>
             </div>
         </div>
+    </section>
 
-        <!-- project section -->
-
-        <div class="project">
-            <div class="project_container">
-                <div class="project_item">
-                    <h1>Our Project</h1>
-                    <p>
-                        Sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Quis
-                        ipsum suspendisse amet, consectetur adipiscing elit, sed
-                        do eiusmod tempor incididunt ut labore et dolore magna
-                        aliqua.
-                    </p>
+    <!-- project section -->
+    <section class="project-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="project_item">
+                        <h1>Our Project</h1>
+                        <p>
+                            Sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna
+                            aliqua. Quis ipsum suspendisse amet, consectetur
+                            adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.
+                        </p>
+                    </div>
                 </div>
-                <img
-                    src="../../images/project-pic-1.jpg"
-                    class="image_project"
-                    alt="project"
-                />
-                <img
-                    src="../../images/project-pic-2.jpg"
-                    class="image_project"
-                    alt="project"
-                />
-                <img
-                    src="../../images/project-pic-3.jpg"
-                    class="image_project"
-                    alt="project"
-                />
-                <img
-                    src="../../images/project-pic-4.jpg"
-                    class="image_project"
-                    alt="project"
-                />
-                <img
-                    src="../../images/project-pic-5.jpg"
-                    class="image_project"
-                    alt="project"
-                />
+                <div class="col-md-4">
+                    <img
+                        src="../../images/project-pic-1.jpg"
+                        class="image_project"
+                        alt="project"
+                    />
+                </div>
+                <div class="col-md-4">
+                    <img
+                        src="../../images/project-pic-2.jpg"
+                        class="image_project"
+                        alt="project"
+                    />
+                </div>
+                <div class="col-md-4">
+                    <img
+                        src="../../images/project-pic-3.jpg"
+                        class="image_project"
+                        alt="project"
+                    />
+                </div>
+                <div class="col-md-4">
+                    <img
+                        src="../../images/project-pic-4.jpg"
+                        class="image_project"
+                        alt="project"
+                    />
+                </div>
+                <div class="col-md-4">
+                    <img
+                        src="../../images/project-pic-5.jpg"
+                        class="image_project"
+                        alt="project"
+                    />
+                </div>
             </div>
         </div>
+    </section>
 
-        <!-- Team section -->
+    <!-- Team section -->
 
-        <div class="team">
-            <h1 class="d-flex justify-content-center align-self-center">
-                Our Team
-            </h1>
+    <div class="team">
+        <h1 class="d-flex justify-content-center align-self-center">
+            Our Team
+        </h1>
 
+        <p>
+            We are a team of UI/UX designers who are passionate about creating
+            beautiful and functional designs. viverra maecenas accumsan lacus
+            vel facilisis. Dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <button class="btn">See More Our Teamwork</button>
+        <div
+            class="service_item d-flex flex-column justify-content-start align-items-center"
+        >
+            <h2>BlueBees AI</h2>
             <p>
                 We are a team of UI/UX designers who are passionate about
-                creating beautiful and functional designs. viverra maecenas
-                accumsan lacus vel facilisis. Dolor sit amet, consectetur
-                adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua.
+                creating beautiful and functional designs.
             </p>
-            <button class="btn">See More Our Teamwork</button>
-            <div
-                class="service_item d-flex flex-column justify-content-start align-items-center"
-            >
-                <h2>BlueBees AI</h2>
-                <p>
-                    We are a team of UI/UX designers who are passionate about
-                    creating beautiful and functional designs.
-                </p>
+        </div>
+    </div>
+
+    <!-- Client section -->
+
+    <div class="client">
+        <h1>Our Clients</h1>
+        <div class="client_container">
+            <div>
+                <img src="../../images/01.png" alt="client" />
+            </div>
+            <div>
+                <img src="../../images/02.png" alt="client" />
+            </div>
+            <div>
+                <img src="../../images/03.png" alt="client" />
+            </div>
+            <div>
+                <img src="../../images/04.png" alt="client" />
+            </div>
+            <div>
+                <img src="../../images/05.png" alt="client" />
+            </div>
+            <div>
+                <img src="../../images/06.png" alt="client" />
+            </div>
+            <div>
+                <img src="../../images/07.png" alt="client" />
+            </div>
+            <div>
+                <img src="../../images/08.png" alt="client" />
+            </div>
+            <div>
+                <img src="../../images/09.png" alt="client" />
+            </div>
+            <div>
+                <img src="../../images/10.png" alt="client" />
+            </div>
+            <div>
+                <img src="../../images/11.png" alt="client" />
+            </div>
+            <div>
+                <img src="../../images/12.png" alt="client" />
             </div>
         </div>
+    </div>
 
-        <!-- Client section -->
-
-        <div class="client">
-            <h1>Our Clients</h1>
-            <div class="client_container">
-                <div>
-                    <img src="../../images/01.png" alt="client" />
-                </div>
-                <div>
-                    <img src="../../images/02.png" alt="client" />
-                </div>
-                <div>
-                    <img src="../../images/03.png" alt="client" />
-                </div>
-                <div>
-                    <img src="../../images/04.png" alt="client" />
-                </div>
-                <div>
-                    <img src="../../images/05.png" alt="client" />
-                </div>
-                <div>
-                    <img src="../../images/06.png" alt="client" />
-                </div>
-                <div>
-                    <img src="../../images/07.png" alt="client" />
-                </div>
-                <div>
-                    <img src="../../images/08.png" alt="client" />
-                </div>
-                <div>
-                    <img src="../../images/09.png" alt="client" />
-                </div>
-                <div>
-                    <img src="../../images/10.png" alt="client" />
-                </div>
-                <div>
-                    <img src="../../images/11.png" alt="client" />
-                </div>
-                <div>
-                    <img src="../../images/12.png" alt="client" />
-                </div>
-            </div>
-        </div>
-
-        <!-- Review section -->
-        <div class="review">
-            <h1>Our Happy Customers</h1>
-        </div>
-        <div class="footer_bg">
-            <!-- <img src="../../images/hero-rev-banner-.jpg" /> -->
-        </div>
+    <!-- Review section -->
+    <div class="review">
+        <h1>Our Happy Customers</h1>
+    </div>
+    <div class="footer_bg">
+        <!-- <img src="../../images/hero-rev-banner-.jpg" /> -->
     </div>
 </template>
