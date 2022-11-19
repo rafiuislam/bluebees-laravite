ed56f3a0f6b43eec83196f848284c3b9516c2068
venturexrepo <mobarakali62@gmail.com>
2022-11-19 16:01:34 +0600

    slider

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,27 +1,9 @@
 <template>
     <footer class="footer">
-        <!-- <footer>
-            <div class="waves">
-              <div class="wave" id="wave1"></div>
-              <div class="wave" id="wave2"></div>
-              <div class="wave" id="wave3"></div>
-              <div class="wave" id="wave4"></div>
-            </div>
-            <ul class="social_icon">
-              <li>
-                <a href="#"><ion-icon name="logo-facebook"></ion-icon></a>
-              </li>
-              <li>
-                <a href="#"><ion-icon name="logo-twitter"></ion-icon></a>
-              </li>
-              <li>
-                <a href="#"><ion-icon name="logo-twitch"></ion-icon></a>
-              </li>
-              <li>
-                <a href="#"><ion-icon name="logo-instagram"></ion-icon></a>
-              </li>
-            </ul>
-          </footer> -->
+        <div class="waves">
+            <div class="wave" id="wave1"></div>
+            <div class="wave" id="wave2"></div>
+        </div>
         <div class="container">
             <div class="footer-col">
                 <span>
--- resources/js/components/HomeSlider.vue
+++ resources/js/components/HomeSlider.vue
@@ -1,146 +1,19 @@
 <template>
-    <!-- <section class="slider">
-        <h1 class="title">Our Happy Customers</h1>
-        <div class="slides">
-            <div class="slide col-md-12">
-                <div class="image">
-                    <img src="../../images/happy-client-icon.png" alt="" />
-                </div>
-                <div class="content text-center">
-                    <div class="slide-text">
-                        Sed tempor eget leo ut gravida. Quisque congue sagittis
-                        purus, sit amet frinfringilla sapien quis, tempor id,
-                        scelerisque Sed tempor eget leo ut gravida. Quisque
-                        congue sagittis purus, sit amet frinfringilla sapien
-                        quis, tempor id, scelerisque Sed tempor eget leo ut
-                        gravida. Quisque congue sagittis purus,
-                    </div>
-                    <p>
-                        <span style="color: #00abe5" class="text-color">
-                            Abdulla,
-                        </span>
-                        Client
-                    </p>
-                </div>
-            </div>
-            <div class="slide col-md-12">
-                <div class="image">
-                    <img src="../../images/happy-client-icon.png" alt="" />
-                </div>
-                <div class="content text-center">
-                    <div class="slide-text">
-                        Sed tempor eget leo ut gravida. Quisque congue sagittis
-                        purus, sit amet frinfringilla sapien quis, tempor id,
-                        scelerisque Sed tempor eget leo ut gravida. Quisque
-                        congue sagittis purus, sit amet frinfringilla sapien
-                        quis, tempor id, scelerisque Sed tempor eget leo ut
-                        gravida. Quisque congue sagittis purus,
-                    </div>
-                    <p>
-                        <span style="color: #00abe5" class="text-color">
-                            Abdulla,
-                        </span>
-                        Client
-                    </p>
-                </div>
-            </div>
-            <div class="slide col-md-12">
-                <div class="image">
-                    <img src="../../images/happy-client-icon.png" alt="" />
-                </div>
-                <div class="content text-center">
-                    <div class="slide-text">
-                        Sed tempor eget leo ut gravida. Quisque congue sagittis
-                        purus, sit amet frinfringilla sapien quis, tempor id,
-                        scelerisque Sed tempor eget leo ut gravida. Quisque
-                        congue sagittis purus, sit amet frinfringilla sapien
-                        quis, tempor id, scelerisque Sed tempor eget leo ut
-                        gravida. Quisque congue sagittis purus,
-                    </div>
-                    <p>
-                        <span style="color: #00abe5" class="text-color">
-                            Abdulla,
-                        </span>
-                        Client
-                    </p>
-                </div>
-            </div>
-            <div class="slide col-md-12">
-                <div class="image">
-                    <img src="../../images/happy-client-icon.png" alt="" />
-                </div>
-                <div class="content text-center">
-                    <div class="slide-text">
-                        Sed tempor eget leo ut gravida. Quisque congue sagittis
-                        purus, sit amet frinfringilla sapien quis, tempor id,
-                        scelerisque Sed tempor eget leo ut gravida. Quisque
-                        congue sagittis purus, sit amet frinfringilla sapien
-                        quis, tempor id, scelerisque Sed tempor eget leo ut
-                        gravida. Quisque congue sagittis purus,
-                    </div>
-                    <p>
-                        <span style="color: #00abe5" class="text-color">
-                            Abdulla,
-                        </span>
-                        Client
-                    </p>
-                </div>
-            </div>
-            <div class="slide col-md-12">
-                <div class="image">
-                    <img src="../../images/happy-client-icon.png" alt="" />
-                </div>
-                <div class="content text-center">
-                    <div class="slide-text">
-                        Sed tempor eget leo ut gravida. Quisque congue sagittis
-                        purus, sit amet frinfringilla sapien quis, tempor id,
-                        scelerisque Sed tempor eget leo ut gravida. Quisque
-                        congue sagittis purus, sit amet frinfringilla sapien
-                        quis, tempor id, scelerisque Sed tempor eget leo ut
-                        gravida. Quisque congue sagittis purus,
-                    </div>
-                    <p>
-                        <span style="color: #00abe5" class="text-color">
-                            Abdulla,
-                        </span>
-                        Client
-                    </p>
-                </div>
-            </div>
-            <div class="slide col-md-12">
-                <div class="image">
-                    <img src="../../images/happy-client-icon.png" alt="" />
-                </div>
-                <div class="content text-center">
-                    <div class="slide-text">
-                        Sed tempor eget leo ut gravida. Quisque congue sagittis
-                        purus, sit amet frinfringilla sapien quis, tempor id,
-                        scelerisque Sed tempor eget leo ut gravida. Quisque
-                        congue sagittis purus, sit amet frinfringilla sapien
-                        quis, tempor id, scelerisque Sed tempor eget leo ut
-                        gravida. Quisque congue sagittis purus,
-                    </div>
-                    <p>
-                        <span style="color: #00abe5" class="text-color">
-                            Abdulla,
-                        </span>
-                        Client
-                    </p>
-                </div>
-            </div>
-        </div>
-    </section> -->
     <section class="slider">
         <h1 class="title">Our Happy Customers</h1>
         <div class="container">
+            <!-- Demo -->
             <Carousel
+                id="thumbnails"
                 :settings="settings"
                 :wrapAround="true"
                 :transition="500"
                 :breakpoints="breakpoints"
+                v-model="currentSlide"
+                ref="carousel"
             >
                 <Slide v-for="slide in 10" :key="slide">
-                    <div class="carousel__item">
+                    <div class="carousel__item" @click="slideTo(slide - 1)">
                         <div class="image">
                             <img
                                 src="../../images/happy-client-icon.png"
@@ -149,10 +22,34 @@
                         </div>
                     </div>
                 </Slide>
-
                 <template #addons>
                     <Navigation />
                 </template>
+            </Carousel>
+
+            <Carousel
+                id="gallery"
+                :items-to-show="1"
+                :wrap-around="true"
+                v-model="currentSlide"
+            >
+                <Slide v-for="slide in 10" :key="slide">
+                    <div class="carousel__item">
+                        <div class="caption">
+                            <p>
+                                {{ slide }} Sed tempor eget leo ut gravida.
+                                Quisque congue sagittis purus, sit amet
+                                frinfringilla sapien quis, tempor id,
+                                scelerisque Sed tempor eget leo ut gravida.
+                                Quisque congue sagittis purus, sit amet
+                                frinfringilla sapien quis, tempor id,
+                                scelerisque Sed tempor eget leo ut gravida.
+                                Quisque congue sagittis purus,
+                            </p>
+                            <h5><span>Abdulla,</span> Client</h5>
+                        </div>
+                    </div>
+                </Slide>
             </Carousel>
         </div>
     </section>
@@ -172,6 +69,7 @@ export default defineComponent({
     },
     data: () => ({
         // carousel settings
+        currentSlide: 0,
         settings: {
             itemsToShow: 1,
             snapAlign: "center",
@@ -191,45 +89,10 @@ export default defineComponent({
             },
         },
     }),
+    methods: {
+        slideTo(val) {
+            this.currentSlide = val;
+        },
+    },
 });
 </script>
-
-<style scoped>
-.carousel__item {
-    width: auto;
-    height: auto;
-}
-
-.carousel__slide {
-    padding: 10px 0px;
-}
-
-.carousel__viewport {
-    perspective: 2000px;
-    width: 100%;
-}
-
-.carousel__slide--sliding {
-    transition: 0.5s;
-}
-
-.carousel__slide {
-    opacity: 0.5;
-    transform: rotateY(-10deg) scale(0.5);
-}
-
-.carousel__slide--prev {
-    opacity: 0.5;
-    transform: rotateY(-10deg) scale(0.7);
-}
-
-.carousel__slide--next {
-    opacity: 0.5;
-    transform: rotateY(10deg) scale(0.7);
-}
-
-.carousel__slide--active {
-    opacity: 1;
-    transform: rotateY(0) scale(1.1);
-}
-</style>
