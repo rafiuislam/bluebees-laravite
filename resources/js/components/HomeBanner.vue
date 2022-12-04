f808078f79382fea49db42b6395abc81ac524728
Rafiu Islam <md.rafiu.islam@g.bracu.ac.bd>
2022-12-04 11:34:26 +0600

    Menu button updated and made circular

--- resources/js/components/HomeBanner.vue
+++ resources/js/components/HomeBanner.vue
@@ -2,11 +2,10 @@
     <div class="home-banner">
         <img src="../../images/banner.png" />
         <div class="banner-caption">
-            <div class="container">
+            <div class="container d-flex">
                 <h2>
-                    BlueBees AI Your Trusted<br />
-                    Software <a class="under"> Development </a>
-                    <br />
+                    BlueBees AI Your Trusted Software
+                    <a class="under"> Development </a>
                     Company
                 </h2>
             </div>
